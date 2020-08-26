4e3dda0bee789b5b36ab1806749e19e708eb7fc4
jjmin321 <jjmin321@naver.com>
2020-08-26 13:21:12 +0900

    [UPDATE] JAVA/프로그래머스/해시/Hash1.java

--- JAVA/프로그래머스/해시/Hash1.java
+++ JAVA/프로그래머스/해시/Hash1.java
@@ -6,28 +6,39 @@ https://programmers.co.kr/learn/courses/30/lessons/42576
 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
 
-풀이 : 
+풀이 : 배열과 맵 두
 */
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Hash1 {
     public static void main(String []args) {
         String[] participant = {"mislav", "stanko", "mislav", "ana"};
-        String[] completion = {"mislav", "ana", "mislav"};
-        String answer = hashmethod(participant, completion);
-        System.out.println(answer.toString());
+        String[] completion = {"stanko", "ana", "mislav"};
+        String answer = solution(participant, completion);
+        System.out.println(answer);
     }
 
-    public static String hashmethod(String[] participant, String[] completion) {
+    public static String solution(String[] participant, String[] completion) {
         Map<String, Integer> cache = new HashMap<>();
         for (int i = 0; i < participant.length; i++) {
-            cache.put(participant[i], 1);
+            if (0 < cache.getOrDefault(participant[i], 0)) {
+                cache.replace(participant[i], cache.get(participant[i]) + 1);
+            }else {
+                cache.put(participant[i], 1);
+            }
         }
         for (int i = 0; i < completion.length; i++) {
-            cache.remove(completion[i]);
+            if (0 < cache.get(completion[i])) {
+                cache.replace(completion[i], cache.get(completion[i]) - 1);
+            }
         }
-        return cache.keySet().toString();
+        for (Map.Entry<String, Integer> item : cache.entrySet()) {
+            if (0 < item.getValue()) 
+                return item.getKey();
+        }
+        return null;
     }
 }
