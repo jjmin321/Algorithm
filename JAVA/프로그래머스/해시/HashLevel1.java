9d4d1fd4dd978fccd02f1192d77a8605c3feb04a
jjmin321 <jjmin321@naver.com>
2020-08-30 21:37:32 +0900

    [UPDATE] JAVA/프로그래머스/해시/HashLevel1.java]

--- JAVA/프로그래머스/해시/HashLevel1.java
+++ JAVA/프로그래머스/해시/HashLevel1.java
@@ -23,17 +23,15 @@ public class HashLevel1 {
 
     public static String solution(String[] participant, String[] completion) {
         Map<String, Integer> cache = new HashMap<>();
-        for (int i = 0; i < participant.length; i++) {
-            if (0 < cache.getOrDefault(participant[i], 0)) {
+        for (int i = 0; i < participant.length; i++) 
+            if (0 < cache.getOrDefault(participant[i], 0)) 
                 cache.replace(participant[i], cache.get(participant[i]) + 1);
-            }else {
+            else 
                 cache.put(participant[i], 1);
-            }
-        }
+
         for (int i = 0; i < completion.length; i++) {
-            if (0 < cache.get(completion[i])) {
+            if (0 < cache.get(completion[i])) 
                 cache.replace(completion[i], cache.get(completion[i]) - 1);
-            }
         }
         for (Map.Entry<String, Integer> item : cache.entrySet()) {
             if (0 < item.getValue()) 
