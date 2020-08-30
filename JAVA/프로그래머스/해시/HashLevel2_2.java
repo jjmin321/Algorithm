ba8451b6f682da1e16ce26ddb8593ff3cc859ce0
jjmin321 <jjmin321@naver.com>
2020-08-30 21:35:47 +0900

    [UPDATE] JAVA/프로그래머스/해시/HashLevel2_2.java

---
+++ JAVA/프로그래머스/해시/HashLevel2_2.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashLevel2_2 {
+    public static void main(String[] args) {
+        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        System.out.println(solution(clothes));
+    }
+
+    public static int solution(String[][] clothes) {
+        int answer = 0;
+        Map<String, Integer> cache = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++){
+            if (0 < cache.getOrDefault(clothes[i][1], 0)) 
+                cache.replace(clothes[i][1], cache.get(clothes[i][1])+1);
+            else 
+                cache.put(clothes[i][1], 1);
+            
+        }
+        return answer;
+    }
+}
