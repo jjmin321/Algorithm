ded9dfbbd4c6ca09ea2bc56571e98535682e0798
jjmin321 <jjmin321@naver.com>
2020-08-30 23:29:03 +0900

    [UPDATE] JAVA/프로그래머스/해시/HashLevel2_2.java

--- JAVA/프로그래머스/해시/HashLevel2_2.java
+++ JAVA/프로그래머스/해시/HashLevel2_2.java
@@ -1,25 +1,38 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HashLevel2_2 {
     public static void main(String[] args) {
-        String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        String[][] clothes = { { "yellow_hat", "headgear" }, { "blue_sunglasses", "eyewear" },
+                { "green_turban", "headgear" } };
         System.out.println(solution(clothes));
     }
 
     public static int solution(String[][] clothes) {
         int answer = 0;
         Map<String, Integer> cache = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++){
+        List<Integer> clothCount = new ArrayList<>();
+        for (int i = 0; i < clothes.length; i++) {
             if (0 < cache.getOrDefault(clothes[i][1], 0)) 
                 cache.replace(clothes[i][1], cache.get(clothes[i][1])+1);
             else 
                 cache.put(clothes[i][1], 1);
         }
-        
-        for (Map.Entry<String, Integer> item : cache.entrySet()) {
-            System.out.println(item);
+        for (Integer item : cache.values()) {
+            clothCount.add(item);
+        }  
+        for (int i = 0; i < clothCount.size(); i++) {
+            if (clothCount.size() == 1)
+                return clothCount.get(0);
+            else if (clothCount.size() == i+1) 
+                break;
+            else if (i == 0)
+                answer += (clothCount.get(i)+1) * (clothCount.get(i+1)+1);
+            else 
+                answer += answer * clothCount.get(i+1)+1;
         }
-        return answer;
+        return answer-(clothCount.size()-1);
     }
 }
