15bb26fe5fd1be10a9d0abbca5decb0efaa84ff8
jjmin321 <jjmin321@naver.com>
2020-08-31 12:26:29 +0900

    [UPDATE] JAVA/프로그래머스/해시/HashLevel2_2.java

--- JAVA/프로그래머스/해시/HashLevel2_2.java
+++ JAVA/프로그래머스/해시/HashLevel2_2.java
@@ -11,9 +11,8 @@ public class HashLevel2_2 {
     }
 
     public static int solution(String[][] clothes) {
-        int answer = 0;
+        int answer = 1;
         Map<String, Integer> cache = new HashMap<>();
-        List<Integer> clothCount = new ArrayList<>();
         for (int i = 0; i < clothes.length; i++) {
             if (0 < cache.getOrDefault(clothes[i][1], 0)) 
                 cache.replace(clothes[i][1], cache.get(clothes[i][1])+1);
@@ -21,18 +20,8 @@ public class HashLevel2_2 {
                 cache.put(clothes[i][1], 1);
         }
         for (Integer item : cache.values()) {
-            clothCount.add(item);
-        }  
-        for (int i = 0; i < clothCount.size(); i++) {
-            if (clothCount.size() == 1)
-                return clothCount.get(0);
-            else if (clothCount.size() == i+1) 
-                break;
-            else if (i == 0)
-                answer += (clothCount.get(i)+1) * (clothCount.get(i+1)+1);
-            else 
-                answer += answer * clothCount.get(i+1)+1;
+            answer = answer * (item+1);
         }
-        return answer-(clothCount.size()-1);
+        return answer-1;
     }
 }
