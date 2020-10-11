0e1b484dbae0606fd389e4bb333543e904ebf75e
jjmin321 <jjmin321@naver.com>
2020-10-11 17:04:49 +0900

    [ADD] SQL/Programmers/

--- SQL/Programmers/아픈 동물찾기.sql
+++ SQL/Programmers/아픈 동물찾기.sql
@@ -1,7 +1,7 @@
 /*
 https://programmers.co.kr/learn/courses/30/lessons/59036
 
-1. SELECT ANIMAL_ID, NAME FROM으로 테이블에서 아이디 값, 이름 값을 가지고 옴 
+1. SELECT ANIMAL_ID, NAME FROM으로 테이블의 아이디, 이름 정보를 가지고 옴 
 2. WHERE으로 아픈 동물들만 찾음 
 3. ORDER BY로 오름차순 정렬함
 */
---
+++ SQL/Programmers/어린 동물찾기.sql
@@ -0,0 +1,6 @@
+/*
+https://programmers.co.kr/learn/courses/30/lessons/59037
+
+1. SELECT * FROM 으로 테이블의 전체 정보를 가져옴 
+2. ORDER BY 로 오름차순 정렬함
+*/
