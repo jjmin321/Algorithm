f222e90b1adb1ddd063088ba679058d2c3cd3f5a
jjmin321 <jjmin321@naver.com>
2020-10-11 17:11:43 +0900

    [ADD] SQL/Programmers/

--- SQL/Programmers/동물의 아이디와 이름.sql
+++ SQL/Programmers/동물의 아이디와 이름.sql
@@ -2,7 +2,7 @@
 https://programmers.co.kr/learn/courses/30/lessons/59403
 
 1. SELECT ANIMAL_ID, NAME FROM 으로 테이블의 아이디, 이름 정보를 가져옴 
-3. ORDER BY 로 오름차순 정렬함
+2. ORDER BY 로 오름차순 정렬함
 */
 
 SELECT ANIMAL_ID, NAME FROM ANIMAL_INS
