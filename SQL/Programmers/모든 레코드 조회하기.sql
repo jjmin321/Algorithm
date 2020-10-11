982da011ddc838c2a36455f8d77d342d481db208
jjmin321 <jjmin321@naver.com>
2020-10-11 16:57:01 +0900

    [ADD] SQL/Programmers/

--- SQL/Programmers/모든 레코드 조회하기.sql
+++ SQL/Programmers/모든 레코드 조회하기.sql
@@ -1,7 +1,8 @@
 /*
 https://programmers.co.kr/learn/courses/30/lessons/59034
 
-풀이 : SELECT * FROM으로 ANIMALS_INS 테이블의 정보를 불러온 후 ORDER BY를 사용하여 ANIMAL_ID 오름차순으로 정렬하였다
+1. SELECT * FROM 으로 테이블의 전체 정보를 가져옴 
+2. ORDER BY 로 오름차순 정렬함
 */
 
 SELECT * FROM ANIMAL_INS ORDER BY ANIMAL_ID
