9d1adbe14225205578ce89e89168623d5b171c93
jjmin321 <jjmin321@naver.com>
2020-10-13 20:34:38 +0900

    [ADD] SQL/Programmers/GroupBy/

---
+++ SQL/Programmers/GroupBy/고양이와 개는 몇 마리 있을까.sql
@@ -0,0 +1,12 @@
+/*
+https://programmers.co.kr/learn/courses/30/lessons/59040
+
+1. SELECT ANIMAL_TYPE, Count as count로 동물의 종류, 동물의 수를 count변수로 가져옴 
+2. GROUP BY ANIMAL_TYPE으로 동물의 종류 별로 동물의 수를 계산함
+3. ORDER BY ANIMAL_TYPE으로 동물의 종류명에 따라 오름차순으로 정렬함
+*/
+
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) as count 
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE
