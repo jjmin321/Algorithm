71b0caa9aa55628cf8a98912fe8f8413b482bd35
jjmin321 <jjmin321@naver.com>
2020-11-14 20:06:22 +0900

    [ADD] PYTHON/Programmers/2019카카오개발자겨울인턴십/크레인 인형뽑기 게임.py

--- PYTHON/Programmers/2019카카오개발자겨울인턴십/크레인 인형뽑기 게임.py
+++ PYTHON/Programmers/2019카카오개발자겨울인턴십/크레인 인형뽑기 게임.py
@@ -1,3 +1,10 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/64061
+
+풀이 : moves 배열을 하나씩 순회하며 2차원 배열로 인형들이 들어있는 board에서 인형을 뽑아 data에 넣습니다.
+      data, 즉 인형 바구니에 인형이 1개 초과로 들어간다면 원래 있던 인형과 새로 들어간 인형을 비교하여 같은 인형일 경우 바구니에서 제거 후 answer에 2를 더했습니다.
+'''
+
 def solution(board, moves) :
     data = []
     answer = 0
