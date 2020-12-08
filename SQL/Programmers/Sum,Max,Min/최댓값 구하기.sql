0283af0331040e372d8678b0a6957e1691397fb4
jjmin321 <jjmin321@naver.com>
2020-12-08 09:04:50 +0900

    [ADD] SQL/Programmers/Sum,Max,Min/

--- SQL/Programmers/Sum,Max,Min/최댓값 구하기.sql
+++ SQL/Programmers/Sum,Max,Min/최댓값 구하기.sql
@@ -1,7 +1,7 @@
 /*
 https://programmers.co.kr/learn/courses/30/lessons/59415
 
-풀이 : 가장 최근에 들어온 동물을 구하기 위해서는 DATETIME중 최댓값을 가져오면 된다. 즉, SELECT MAX(DATETIME) FROM ANIMAL_INS 를 하면 DATETIME중 가장 큰 값을 가져온다.
+풀이 : 가장 최근에 들어온 동물을 구하기 위해서는 DATETIME중 최댓값을 가져오면 된다. 즉, SELECT MAX(DATETIME) FROM ANIMAL_INS 를 하면 DATETIME중 가장 큰 값을 가져옴
 */
 
 SELECT MAX(DATETIME) FROM ANIMAL_INS
