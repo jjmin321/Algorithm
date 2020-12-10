239275ae8e9f341b8b8e496bd291d5064bd265ce
jjmin321 <jjmin321@naver.com>
2020-12-10 16:12:55 +0900

    [ADD] Python/Programmers/연습문제/

--- PYTHON/Programmers/연습문제/수박수박수박수박수박수?.py
+++ PYTHON/Programmers/연습문제/수박수박수박수박수박수?.py
@@ -1,7 +1,7 @@
 '''
-https://programmers.co.kr/learn/courses/30/lessons/12921
+https://programmers.co.kr/learn/courses/30/lessons/12922
 
-풀이 : 0부터 n미만까지 반복하여 0과 짝수일 때는 
+풀이 : 0부터 n미만까지 반복하여 0과 짝수일 때는 수, 홀수일 때는 박을 추가한 문자열을 반환하였습니다.
 '''
 
 def solution(n):
