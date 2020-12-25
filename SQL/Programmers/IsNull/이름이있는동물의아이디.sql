59453f17409279980cdcf67ad3de6ea34482734b
jjmin321 <jjmin321@naver.com>
2020-12-25 14:42:29 +0900

    [ADD] SQL/Programmers/IsNull/

---
+++ SQL/Programmers/IsNull/이름이있는동물의아이디.sql
@@ -0,0 +1,9 @@
+/*
+https://programmers.co.kr/learn/courses/30/lessons/59407
+
+풀이 : WHERE을 사용하여 NAME이 null이 아닌 ANIMAL_ID를 가져왔고 ORDER BY를 사용해 오름차순으로 정렬하였습니다.
+*/
+
+SELECT ANIMAL_ID FROM ANIMAL_INS
+WHERE NAME is not null
+ORDER BY ANIMAL_ID
