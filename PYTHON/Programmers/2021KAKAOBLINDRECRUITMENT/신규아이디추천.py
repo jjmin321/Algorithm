4fb1e03b6dd7ca64049a281bae7d6d1982c3cc2b
jjmin321 <jjmin321@naver.com>
2021-03-03 10:59:23 +0900

    [ADD] PYTHON/Programmers/2021KAKAOBLINDRECRUITMENT/신규아이디추천.py

--- PYTHON/Programmers/2021KAKAOBLINDRECRUITMENT/신규아이디추천.py
+++ PYTHON/Programmers/2021KAKAOBLINDRECRUITMENT/신규아이디추천.py
@@ -4,14 +4,16 @@ https://programmers.co.kr/learn/courses/30/lessons/72410
 풀이 : 정규표현식을 사용하여 문자에 관련된 규칙들을 해결하였습니다.
 '''
 
+import re
+
 def solution(new_id):
     answer = new_id.lower()
     answer = re.sub('[^a-z0-9\-_.]', '', answer)
     answer = re.sub('\.+', '.', answer)
     answer = re.sub('^[.]|[.]$', '', answer)
-    answer = 'a' if len answer) == 0 else answer[:15]
-    answer = re.sub('^[.]|[.]$', '' answert)
-    answer = answer if len(answer) > 2 else answer + "".join( answer[-1] for i in range(3-len(answer))])
+    answer = 'a' if len (answer) == 0 else answer[:15]
+    answer = re.sub('^[.]|[.]$', '', answer)
+    answer = answer if len(answer) > 2 else answer + "".join( answer[-1] for i in range(3-len(answer)))
     return answer 
 
 new_id = "z-+.^."
