3203585b910a8405c9e6716fa1523c6532b54773
jjmin321 <jjmin321@naver.com>
2021-03-23 10:25:04 +0900

    Add: 알고리즘연습장/DFS, BFS 문제

---
+++ PYTHON/알고리즘연습장/미로탈출.py
@@ -0,0 +1,28 @@
+from collections import deque 
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i] 
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+    return graph[n-1][m-1]
+
+    
+
+n, m = map(int, input().split())
+graph = [] # 미로의 정보가 들어갈 그래프
+for i in range(n):
+    graph.append(list(map(int, input()))) 
+dx = [-1, 1, 0, 0] # 그래프의 첫 번째 인덱스 상, 하, 좌, 우 
+dy = [0, 0, -1, 1] # 그래프의 두 번째 인덱스 상, 하, 좌, 우
+print(bfs(0, 0))
