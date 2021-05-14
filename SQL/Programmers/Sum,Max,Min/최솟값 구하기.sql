5a1e91ea44105bbba0bd44ba52538b7e09664d8c
jjmin321 <jjmin321@naver.com>
2021-05-14 09:19:26 +0900

    [ADD] SQL/Programmers/Sum%2CMax%2CMin/최솟값%20구하기.sql

---
+++ SQL/Programmers/Sum,Max,Min/최솟값 구하기.sql
@@ -0,0 +1,7 @@
+/*
+https://programmers.co.kr/learn/courses/30/lessons/59038
+
+풀이 : SELECT DATETIME FROM ANIMAL_INS로 테이블 내의 시간을 가져올 수 있는데 그 중, min 함수를 통해 최솟값만을 가져올 수 있다.
+*/
+
+SELECT MIN(DATETIME) FROM ANIMAL_INS
